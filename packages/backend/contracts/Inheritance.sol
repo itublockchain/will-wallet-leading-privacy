52e5ab2a3c956e2132b49e032b24a9c042c5c87d
Ali Eray <alieraay@gmail.com>
2023-04-28 21:11:34 +0300

    Inheritance adjusted

--- packages/backend/contracts/Inheritance.sol
+++ packages/backend/contracts/Inheritance.sol
@@ -3,12 +3,7 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-/* 
-Inheritence Account Abstraction
- */
 contract Inheritance {
-
-
     address public owner;
     uint256 public lockDuration;
     uint256 public totalShares;
@@ -16,110 +11,81 @@ contract Inheritance {
 
     struct Beneficiary {
         address beneficiary;
-        uint256 sharesPercent;
+        uint256 shares;
     }
 
-    Beneficiary[] public beneficiaries;
+    mapping(address => Beneficiary) public beneficiariesMap;
+    address[] public beneficiaryAddresses;
 
-    event FundsClaimed(
-        address indexed beneficiary,
-        uint256 amount
-        );
+    event FundsClaimed(address indexed beneficiary, uint256 amount);
 
-
-    modifier onlyAccount() {
-        require(
-            msg.sender == address(this),
-            "Only the account that inherits this contract can call this method."
-        );
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this method.");
         _;
     }
 
-
-    constructor(
-        address _owner
-    ) {
+    constructor(address _owner) {
         owner = _owner;
         lastActive = block.timestamp;
     }
 
+    function addOrUpdateBeneficiary(address _beneficiary, uint256 _shares) external onlyOwner {
+        require(_shares > 0, "Shares must be greater than 0");
+        require(_shares <= 100, "Shares must be less than or equal to 100");
 
-    function addBeneficiary(
-        address _beneficiary,
-        uint256 _sharesPercent
-    ) external onlyAccount {
-        require(
-            _sharesPercent > 0,
-            "Share percentage must be greater than 0"
-        );
-        require(
-            _sharesPercent <= 100,
-            "Share percentage must be less than or equal to 100"
-        );
-        require(
-            totalShares + _sharesPercent <= 100,
-            "Total shares must be less than or equal to 100"
-        );
+        if (beneficiariesMap[_beneficiary].beneficiary == address(0)) {
+            beneficiaryAddresses.push(_beneficiary);
+        } else {
+            totalShares -= beneficiariesMap[_beneficiary].shares;
+        }
 
-        beneficiaries.push(Beneficiary(_beneficiary, _sharesPercent));
-        totalShares += _sharesPercent;
+        beneficiariesMap[_beneficiary] = Beneficiary(_beneficiary, _shares);
+        totalShares += _shares;
+
+        require(totalShares <= 100, "Total shares must be less than or equal to 100");
     }
 
+    function removeBeneficiary(address _beneficiary) external onlyOwner {
+        require(beneficiariesMap[_beneficiary].beneficiary != address(0), "Beneficiary not found");
 
-    function removeBeneficiary(
-        address _beneficiary
-    ) external onlyAccount {
-        for (uint256 i = 0; i < beneficiaries.length; i++) {
-            if (beneficiaries[i].beneficiary == _beneficiary) {
-                totalShares -= beneficiaries[i].sharesPercent;
-                delete beneficiaries[i];
+        totalShares -= beneficiariesMap[_beneficiary].shares;
+        delete beneficiariesMap[_beneficiary];
+
+        for (uint256 i = 0; i < beneficiaryAddresses.length; i++) {
+            if (beneficiaryAddresses[i] == _beneficiary) {
+                beneficiaryAddresses[i] = beneficiaryAddresses[beneficiaryAddresses.length - 1];
+                beneficiaryAddresses.pop();
+                break;
             }
         }
     }
 
-
-    function updateBeneficiary(
-        address _beneficiary,
-        uint256 _sharesPercent
-    ) external onlyAccount {
-        for (uint256 i = 0; i < beneficiaries.length; i++) {
-            if (beneficiaries[i].beneficiary == _beneficiary) {
-                totalShares -= beneficiaries[i].sharesPercent;
-                beneficiaries[i].sharesPercent = _sharesPercent;
-                totalShares += _sharesPercent;
-            }
-        }
-    }
-
-
-    function updateLastActive() external {
-        require(msg.sender == owner, "Only the owner can update activity");
+    function updateLastActive() external onlyOwner {
         lastActive = block.timestamp;
     }
 
-
     function claimFunds(address _token) external {
-        require(
-            block.timestamp >= lastActive + lockDuration,
-            "Account is still active"
-        );
-        require(
-            beneficiaryShares[msg.sender] > 0,
-            "Sender is not a beneficiary"
-        );
+        require(block.timestamp >= lastActive + lockDuration, "Account is still active");
+        require(beneficiariesMap[msg.sender].beneficiary != address(0), "Sender is not a beneficiary");
 
         uint256 tokenBalance = IERC20(_token).balanceOf(address(this));
-        uint256 shareAmount = (tokenBalance * beneficiaryShares[msg.sender]) /
-            totalShares;
+        uint256 shareAmount = (tokenBalance * beneficiariesMap[msg.sender].shares) / totalShares;
 
         IERC20(_token).transfer(msg.sender, shareAmount);
         emit FundsClaimed(msg.sender, shareAmount);
     }
 
-
-    function changeLockDuration(uint256 _newLockDuration) external {
-        require(msg.sender == owner, "Only the owner can change lock duration");
+    function changeLockDuration(uint256 _newLockDuration) external onlyOwner {
         lockDuration = _newLockDuration;
     }
 
+    function getBeneficiaries() external view returns (Beneficiary[] memory) {
+        Beneficiary[] memory beneficiariesList = new Beneficiary[](beneficiaryAddresses.length);
+
+        for (uint256 i = 0; i < beneficiaryAddresses.length; i++) {
+            beneficiariesList[i] = beneficiariesMap[beneficiaryAddresses[i]];
+        }
+
+        return beneficiariesList;
+    }
 }

3483f36788e37d07333b1b81312f43af5ff4b049
Utku Ömer <omerleroyunda@gmail.com>
2023-04-28 09:34:15 +0300

    setting config

---
+++ packages/backend/hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
+const config: HardhatUserConfig = {
+  zksolc: {
+    version: "1.3.10",
+    compilerSource: "binary",
+    settings: {
+      isSystem: true,
+    },
+  },
+  defaultNetwork: "zkSyncTestnet",
+  networks: {
+    hardhat: {
+      zksync: true,
+    },
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.16",
+  },
+};
+
+export default config;

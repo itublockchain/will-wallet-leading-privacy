f227d77b116caa8becc896a56321f5f645cb5d74
Utku Ömer <omerleroyunda@gmail.com>
2023-04-28 15:39:18 +0300

    Adding functions to inheritance

--- packages/backend/contracts/Inheritance.sol
+++ packages/backend/contracts/Inheritance.sol
@@ -10,11 +10,11 @@ contract Inheritance {
 
 
     address public owner;
-    uint256 public lockDuration;
     address[] public beneficiaries;
-    mapping(address => uint256) public beneficiaryShares;
+    uint256 public lockDuration;
     uint256 public totalShares;
     uint256 public lastActive;
+    mapping(address => uint256) public beneficiaryShares;
 
 
     event FundsClaimed(
@@ -31,32 +31,86 @@ contract Inheritance {
         _;
     }
 
+
     constructor(
-        address _owner,
-        uint256 _lockDuration,
-        address[] memory _beneficiaries,
-        uint256[] memory _shares
+        address _owner
     ) {
+        owner = _owner;
+        lastActive = block.timestamp;
+    }
+
+
+    function setBeneficiaries (
+        address[] memory _beneficiaries,
+        uint256[] memory _sharesPercent
+    ) external onlyAccount {
         require(
-            _beneficiaries.length == _shares.length,
+            _beneficiaries.length == _sharesPercent.length,
             "Beneficiaries and shares arrays must have the same length"
         );
 
-        owner = _owner;
-        lockDuration = _lockDuration;
-        lastActive = block.timestamp;
-
         for (uint256 i = 0; i < _beneficiaries.length; i++) {
             beneficiaries.push(_beneficiaries[i]);
-            beneficiaryShares[_beneficiaries[i]] = _shares[i];
-            totalShares += _shares[i];
+            beneficiaryShares[_beneficiaries[i]] = _sharesPercent[i];
+            totalShares += _sharesPercent[i];
+            require(
+                _sharesPercent[i] > 0,
+                "Share percentage must be greater than 0"
+            );
+            require(
+                _sharesPercent[i] <= 100,
+                "Share percentage must be less than or equal to 100"
+            );
+            require(
+                totalShares <= 100,
+                "Total shares must be less than or equal to 100"
+            );
         }
     }
 
+
+    function removeBeneficiary(address _beneficiary) external onlyAccount {
+        require(
+            beneficiaryShares[_beneficiary] > 0,
+            "Beneficiary does not exist"
+        );
+        totalShares -= beneficiaryShares[_beneficiary];
+        beneficiaryShares[_beneficiary] = 0;
+    }
+
+
+    function changeBeneficiaryShares(
+        address[] memory _beneficiaries,
+        uint256[] memory _sharesPercent
+    ) external onlyAccount {
+        require(
+            _beneficiaries.length == _sharesPercent.length,
+            "Beneficiaries and shares arrays must have the same length"
+        );
+
+        for (uint256 i = 0; i < _beneficiaries.length; i++) {
+            totalShares -= beneficiaryShares[_beneficiaries[i]];
+            beneficiaryShares[_beneficiaries[i]] = _sharesPercent[i];
+            totalShares += _sharesPercent[i];
+            require(
+                _sharesPercent[i] > 0,
+                "Share percentage must be greater than 0"
+            );
+            require(
+                _sharesPercent[i] <= 100,
+                "Share percentage must be less than or equal to 100"
+            );
+            require(
+                totalShares <= 100,
+                "Total shares must be less than or equal to 100"
+            );
+        }
+    }
     function updateLastActive() external {
         require(msg.sender == owner, "Only the owner can update activity");
         lastActive = block.timestamp;
     }
+
 
     function claimFunds(address _token) external {
         require(
@@ -74,5 +128,27 @@ contract Inheritance {
 
         IERC20(_token).transfer(msg.sender, shareAmount);
         emit FundsClaimed(msg.sender, shareAmount);
+    }
+
+
+    function changeLockDuration(uint256 _newLockDuration) external {
+        require(msg.sender == owner, "Only the owner can change lock duration");
+        lockDuration = _newLockDuration;
+    }
+
+
+    function changeBeneficiaryShares(
+        address[] memory _beneficiaries,
+        uint256[] memory _shares
+    ) external {
+        require(msg.sender == owner, "Only the owner can change beneficiaries");
+        require(
+            _beneficiaries.length == _shares.length,
+            "Beneficiaries and shares arrays must have the same length"
+        );
+
+        for (uint256 i = 0; i < _beneficiaries.length; i++) {
+            beneficiaryShares[_beneficiaries[i]] = _shares[i];
+        }
     }
 }

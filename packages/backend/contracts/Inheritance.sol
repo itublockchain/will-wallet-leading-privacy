43bc15a1dd8549d670ffd544c9c6b367d40bf02e
Ali Eray <alieraay@gmail.com>
2023-04-29 04:08:02 +0300

    bug fixed

--- packages/backend/contracts/Inheritance.sol
+++ packages/backend/contracts/Inheritance.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@matterlabs/zksync-contracts/l2/system-contracts/openzeppelin/token/ERC20/IERC20.sol";
 
 contract Inheritance {
-    address public owner;
     uint256 public lockDuration;
     uint256 public totalShares;
     uint256 public lastActive;
@@ -20,16 +19,20 @@ contract Inheritance {
     event FundsClaimed(address indexed beneficiary, uint256 amount);
 
     modifier onlyAccount() {
-        require(msg.sender == address(this), "Only the Account can call this method.");
+        require(
+            msg.sender == address(this),
+            "Only the Account can call this method."
+        );
         _;
     }
-
-    constructor(address _owner) {
-        owner = _owner;
+    constructor() {
         lastActive = block.timestamp;
     }
 
-    function addOrUpdateBeneficiary(address _beneficiary, uint256 _shares) external onlyAccount {
+    function addOrUpdateBeneficiary(
+        address _beneficiary,
+        uint256 _shares
+    ) external onlyAccount {
         require(_shares > 0, "Shares must be greater than 0");
         require(_shares <= 100, "Shares must be less than or equal to 100");
 
@@ -42,18 +45,26 @@ contract Inheritance {
         beneficiariesMap[_beneficiary] = Beneficiary(_beneficiary, _shares);
         totalShares += _shares;
 
-        require(totalShares <= 100, "Total shares must be less than or equal to 100");
+        require(
+            totalShares <= 100,
+            "Total shares must be less than or equal to 100"
+        );
     }
 
     function removeBeneficiary(address _beneficiary) external onlyAccount {
-        require(beneficiariesMap[_beneficiary].beneficiary != address(0), "Beneficiary not found");
+        require(
+            beneficiariesMap[_beneficiary].beneficiary != address(0),
+            "Beneficiary not found"
+        );
 
         totalShares -= beneficiariesMap[_beneficiary].shares;
         delete beneficiariesMap[_beneficiary];
 
         for (uint256 i = 0; i < beneficiaryAddresses.length; i++) {
             if (beneficiaryAddresses[i] == _beneficiary) {
-                beneficiaryAddresses[i] = beneficiaryAddresses[beneficiaryAddresses.length - 1];
+                beneficiaryAddresses[i] = beneficiaryAddresses[
+                    beneficiaryAddresses.length - 1
+                ];
                 beneficiaryAddresses.pop();
                 break;
             }
@@ -65,11 +76,18 @@ contract Inheritance {
     }
 
     function claimFunds(address _token) external {
-        require(block.timestamp >= lastActive + lockDuration, "Account is still active");
-        require(beneficiariesMap[msg.sender].beneficiary != address(0), "Sender is not a beneficiary");
+        require(
+            block.timestamp >= lastActive + lockDuration,
+            "Account is still active"
+        );
+        require(
+            beneficiariesMap[msg.sender].beneficiary != address(0),
+            "Sender is not a beneficiary"
+        );
 
         uint256 tokenBalance = IERC20(_token).balanceOf(address(this));
-        uint256 shareAmount = (tokenBalance * beneficiariesMap[msg.sender].shares) / totalShares;
+        uint256 shareAmount = (tokenBalance *
+            beneficiariesMap[msg.sender].shares) / totalShares;
 
         IERC20(_token).transfer(msg.sender, shareAmount);
         emit FundsClaimed(msg.sender, shareAmount);
@@ -80,7 +98,9 @@ contract Inheritance {
     }
 
     function getBeneficiaries() external view returns (Beneficiary[] memory) {
-        Beneficiary[] memory beneficiariesList = new Beneficiary[](beneficiaryAddresses.length);
+        Beneficiary[] memory beneficiariesList = new Beneficiary[](
+            beneficiaryAddresses.length
+        );
 
         for (uint256 i = 0; i < beneficiaryAddresses.length; i++) {
             beneficiariesList[i] = beneficiariesMap[beneficiaryAddresses[i]];

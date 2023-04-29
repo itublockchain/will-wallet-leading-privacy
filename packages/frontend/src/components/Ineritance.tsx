07a1faddac0e9bacd2c8c6877796d48b91c365cb
oemerfurkan <omerfurkanyuruk1@gmail.com>
2023-04-30 01:52:05 +0300

    Inheritance page

--- packages/frontend/src/components/Ineritance.tsx
+++ packages/frontend/src/components/Ineritance.tsx
@@ -10,27 +10,27 @@ const Inheritance = () => {
             </div>
           </div>
           <div className="mb-1 flex justify-center items-center rounded-full w-[340px] h-[31px] bg-gradient-to-b from-[#18CCC2] to-[#4C75A1]">
-            <input placeholder="Beneficier" className="text-lg text-white rounded-full focus:bg-[#1a6075] bg-[#236c83] w-[336px] h-[27px] pl-3 border-none focus:outline-none"/>
+            <input placeholder="Beneficier" className="font-bold text-lg text-white rounded-full focus:bg-[#1a6075] bg-[#236c83] w-[336px] h-[27px] pl-3 border-none focus:outline-none"/>
           </div>
           <div className="mb-1 flex justify-center items-center rounded-full w-[340px] h-[31px] bg-gradient-to-b from-[#18CCC2] to-[#4C75A1]">
-            <input placeholder="Share" className="text-lg text-white rounded-full focus:bg-[#1a6075] bg-[#236c83] w-[336px] h-[27px] pl-3 border-none focus:outline-none"/>
+            <input placeholder="Percentage" className="font-bold text-lg text-white rounded-full focus:bg-[#1a6075] bg-[#236c83] w-[336px] h-[27px] pl-3 border-none focus:outline-none"/>
           </div>
-          <div className="mb-1 flex justify-center items-center rounded-full w-[100px] h-[31px] bg-gradient-to-b from-[#18CCC2] to-[#4C75A1] m-auto">
-            <button className="text-[12px] text-white rounded-full bg-[#3C93AD] w-[96px] h-[27px]">
+          <div className="mb-1 flex justify-center items-center rounded-full w-[340px] h-[31px] bg-gradient-to-b from-[#18CCC2] to-[#4C75A1] m-auto">
+            <button className="font-bold text-[12px] text-white rounded-full bg-[#3C93AD] w-[336px] h-[27px]">
               Set Beneficiary
             </button>
           </div>
           <div className="mb-1 flex justify-center items-center rounded-full w-[340px] h-[31px] bg-gradient-to-b from-[#18CCC2] to-[#4C75A1]">
-            <input placeholder="Time" className="text-lg text-white rounded-full focus:bg-[#1a6075] bg-[#236c83] w-[336px] h-[27px] pl-3 border-none focus:outline-none"/>
+            <input placeholder="Time" className="font-bold text-lg text-white rounded-full focus:bg-[#1a6075] bg-[#236c83] w-[336px] h-[27px] pl-3 border-none focus:outline-none"/>
           </div>
-          <div className="mb-1 flex justify-center items-center rounded-full w-[134px] h-[31px] bg-gradient-to-b from-[#18CCC2] to-[#4C75A1] m-auto">
-            <button className="text-[12px] text-white rounded-full bg-[#3C93AD] w-[130px] h-[27px]">
+          <div className="mb-1 flex justify-center items-center rounded-full w-[340px] h-[31px] bg-gradient-to-b from-[#18CCC2] to-[#4C75A1] m-auto">
+            <button className="font-bold text-[12px] text-white rounded-full bg-[#3C93AD] w-[336px] h-[27px]">
               Set Countdown Time
             </button>
           </div>
           <div className="mb-1 flex justify-center items-center rounded-full w-[340px] h-[50px] bg-gradient-to-b from-[#18CCC2] to-[#4C75A1] m-auto">
             <button className="text-xl font-bold text-white rounded-full bg-[#3C93AD] w-[336px] h-[46px]">
-              Set Countdown Time
+              I'M ALIVE
             </button>
           </div>
         </div>
--- packages/frontend/src/routes/addbeneficiaries.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from "~components"
-
-const AddBeneficiaries = () => {
-    return <>
-    <div className="w-full h-full">
-
-    </div>
-    </>
-}
